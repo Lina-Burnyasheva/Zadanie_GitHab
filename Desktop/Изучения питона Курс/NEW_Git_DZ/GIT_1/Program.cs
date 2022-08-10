513c5dc37287b34cc2d139c9510c7517264e21ba
Angelina <a.burnyasheva@inbox.ru>
2022-08-10 18:27:11 +0300

    Defined the cycle of passage

--- Desktop/Изучения питона Курс/NEW_Git_DZ/GIT_1/Program.cs
+++ Desktop/Изучения питона Курс/NEW_Git_DZ/GIT_1/Program.cs
@@ -1,2 +1,11 @@
 ﻿string[] array1 = new string[6] {"hello","2", "world",":-)", "1234","1567"};
 string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)//пока длина массива будет больше i будет выполняться этот цикл
+    {
+    
+    }
+}
