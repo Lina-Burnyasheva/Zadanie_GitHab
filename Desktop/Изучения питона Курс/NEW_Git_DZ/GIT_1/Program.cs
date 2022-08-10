08846468520263a0119ac3f8c8499f9b8c80bf62
Angelina <a.burnyasheva@inbox.ru>
2022-08-10 18:29:27 +0300

    Selection condition

--- Desktop/Изучения питона Курс/NEW_Git_DZ/GIT_1/Program.cs
+++ Desktop/Изучения питона Курс/NEW_Git_DZ/GIT_1/Program.cs
@@ -6,6 +6,10 @@ void SecondArrayWithIF(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)//пока длина массива будет больше i будет выполняться этот цикл
     {
-    
+    if(array1[i].Length <= 3)// условие при котором мы будем выводить элемент массива 
+        {
+        array2[count] = array1[i];
+        count++;
+        }
     }
 }
